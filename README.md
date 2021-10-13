806de27f344c25cc8a372d41528564749319d3d5
Stephen Souness <Sounie@users.noreply.github.com>
2021-10-13 15:25:33 +1300

    Removing comments that were helpful to get me started with pycharm, but don't have wider use or general meaning to other users who may take an interest in this code.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-Teaching myself a little bit of Python, so here's a project
-with a Docker container for a postgres database to play with.
+Teaching myself a bit of Python, so here's a project with a Docker container for a postgres database to play with.
 
 Do the following to spin up a Postgresql database in a Docker container before running:
 
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import psycopg
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def read_from_db():
     with psycopg.connect("host=localhost dbname=docker user=docker password=my-local-pass") as conn:
         with conn.cursor() as cur:
@@ -12,6 +8,5 @@ def read_from_db():
 
             print(f'Hi, {result[0]}')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     read_from_db()
